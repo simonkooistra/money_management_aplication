ac9b25f55fee538e6c99cc11bc8ba9128babcfbb
simonkooistra <97022945@st.deltion.nl>
2025-01-09 14:46:21 +0100

    resource routes added to web.php for controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\GroupSavingController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserCategoryController;
+use App\Http\Controllers\UserSavingController;
+use App\Models\GroupCategory;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('GroupCategory', GroupCategory::class);
+Route::resource('Group', GroupController::class);
+Route::resource('GroupSaving', GroupSavingController::class);
+Route::resource('Transaction', TransactionController::class);
+Route::resource('UserCategory', UserCategoryController::class);
+Route::resource('UserSaving', UserSavingController::class);
+
