63e2632ef6bca92572d8fddb180d9f992897c38f
Sean Zwetsloot <szwetslo8267@student.landstede.nl>
2025-01-10 13:04:32 +0100

    Register page gemaakt Inlog page gemaakt Homepage gemaakt.

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+@vite(['resources/css/app.css'])
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aanmelden</title>
+</head>
+
+<body>
+<form action="{{ route('login') }}" method="post">
+    @csrf
+
+    <div class="container">
+        <h1>Aanmelden</h1>
+
+        <input type="text" placeholder="Email Address" name="email" required>
+        @error('email')
+        <span class="text-message">{{ $message }}</span>
+        @enderror
+
+
+        <input type="password" placeholder="Wachtwoord" name="password" required>
+        @error('password')
+        <span class="text-message">{{ $message }}</span>
+        @enderror
+
+        <button type="submit">Aanmelden</button>
+    </div>
+
+    <div class="container">
+
+        <a href="#">Wachtwoord vergeten?</a>
+    </div>
+</form>
+</body>
+
+</html>
