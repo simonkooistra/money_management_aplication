cba1406357ba682ac3c8d87cdeb80197ae5f0f66
Sean Zwetsloot <szwetslo8267@student.landstede.nl>
2025-01-27 15:31:02 +0100

    Register page gemaakt Inlog page gemaakt Homepage gemaakt.

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -4,28 +4,27 @@
 
 @section('content')
 
-    <table class="transaction">
-        <h3>{{auth()->user()->name}} transactions table </h3>
+    <h3>{{ auth()->user()->name }} transactions table</h3>
 
-        @if ($transactions->isEmpty())
-            <p>No transactions yet. make one today!</p>
-            <a href="{{route('transaction.create')}}">Add a transaction</a>
-        @else
+    @if ($transactions->isEmpty())
+        <p>No transactions yet. Make one today!</p>
+        <a href="{{ route('transaction.create') }}">Add a transaction</a>
+    @else
+        <table class="transaction">
             <tr>
-                <th>saving name</th>
-                <th>transaction name</th>
-                <th>created at</th>
-                <th>add at</th>
-                <th>amount</th>
+                <th>Saving Name</th>
+                <th>Transaction Name</th>
+                <th>Created At</th>
+                <th>Amount</th>
                 <th></th>
                 <th></th>
             </tr>
-            @foreach($transactions as $transaction)
+            @foreach ($transactions as $transaction)
                 <tr>
-                    <td>{{$transaction->userSaving->name}}</td>
-                    <td>{{$transaction->name}}</td>
-                    <td>{{$transaction->created_at}}</td>
-                    <td>{{$transaction->amount}}</td>
+                    <td>{{ $transaction->userSaving->name }}</td>
+                    <td>{{ $transaction->name }}</td>
+                    <td>{{ $transaction->created_at }}</td>
+                    <td>&#8364;{{ $transaction->amount }}</td>
                     <td><a href="{{ route('transaction.edit', $transaction->id) }}">Edit</a></td>
                     <td>
                         <form action="{{ route('transaction.destroy', $transaction->id) }}" method="post">
@@ -36,18 +35,9 @@
                     </td>
                 </tr>
             @endforeach
+        </table>
+    @endif
 
-            @if($transactions->count() === 0)
-                <ul>
-                    <li>
-                        <p>No transaction yet. Start with making transaction now!</p>
-                    </li>
-                    <li>
-                        <a href="{{ route('transaction.create') }}">make first transaction?</a>
-                    </li>
-                </ul>
-            @endif
-{{--        @endif--}}
-    </table>
     <a href="{{ route('transaction.create') }}">Create a transaction</a>
+
 @endsection
